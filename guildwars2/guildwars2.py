65e95308d393cf915d23a3d8afa7fcd5ecc739da
fabian.ihle <fabian.ihle@hfwu.de>
2017-03-17 11:30:05 +0100

    Added getIconURL

--- guildwars2/guildwars2.py
+++ guildwars2/guildwars2.py
@@ -410,17 +410,17 @@ class GuildWars2:
                 if gear[piece]["infusions"]:
                     infusion = await self._get_item_name_(gear[piece]["infusions"])
                 if gear[piece]["upgrades"] and not gear[piece]["infusions"]:
-                    msg = " TEST1 {0} {1} with {2}".format(
+                    msg = "TEST1 {0} {1} with {2}".format(
                         statname, itemname, upgrade)
                 elif gear[piece]["upgrades"] and gear[piece]["infusions"]:
-                    msg = " TEST2 {0} {1} with {2} and {3}".format(
+                    msg = "TEST2 {0} {1} with {2} and {3}".format(
                         statname, itemname, upgrade, infusion)
                 elif gear[piece]["infusions"] and not gear[piece]["upgrades"]:
-                    msg = " TEST3 {0} {1} with {2}".format(
+                    msg = "TEST3 {0} {1} with {2}".format(
                         statname, itemname, infusion)
                 elif not gear[piece]["upgrades"] and not gear[piece]["infusions"]:
-                    msg = " Test4 {0} {1}".format(statname, itemname)
-                data.add_field(name=piece, value="iconurl+" "+msg", inline=False)
+                    msg = "Test4 {0} {1}".format(statname, itemname)
+                data.add_field(name=piece, value=iconurl + " " +str(msg)", inline=False)
         data.set_author(name=character)
         data.set_footer(text="A level {0} {1} ".format(
             level, profession.lower()), icon_url=icon)
