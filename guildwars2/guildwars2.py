9bc3da1e11c7f1131eaa56f00c2d14773b46e2b1
n1tr0-5urf3r <Fabian_Ihle@t-online.de>
2017-03-22 20:12:46 +0100

    Works as expected, removing debug info and cleaning up

--- guildwars2/guildwars2.py
+++ guildwars2/guildwars2.py
@@ -758,14 +758,13 @@ class GuildWars2:
                     # associate order from /ranks with rank from /members
                     for rank in ranks:
                         if member_rank == rank['id']:
-                            await self.bot.say('DEBUG: ' + member['name'] + ' has rank ' + member_rank + ' and rank has order ' + str(rank['order']))
+                            #await self.bot.say('DEBUG: ' + member['name'] + ' has rank ' + member_rank + ' and rank has order ' + str(rank['order']))
                             if rank['order'] == order_id:
                              if counter < 20:
                                     data.add_field(name=member['name'], value=member['rank'])
                                     counter = counter + 1
             order_id = order_id + 1
 
-                    #data.add_field(name=member['name'], value=member['rank'])
         try:
             await self.bot.say(embed=data)
         except discord.HTTPException:
