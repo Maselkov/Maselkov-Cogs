0690292076e4bb223ce9570cae5a7eb4bdb7ffa9
fabian.ihle <fabian.ihle@hfwu.de>
2017-03-30 14:19:05 +0200

    Copy paste fault

--- guildwars2/guildwars2.py
+++ guildwars2/guildwars2.py
@@ -845,20 +845,6 @@ class GuildWars2:
                     item_counter += 1
         else:
             await self.bot.say("Treasury is empty!")
-        for item in treasury:
-            if counter < 20:
-                current = item["count"]
-                item_name = itemlist[item_counter]["name"]
-                needed = item["needed_by"]
-
-                for need in needed:
-                    amount = amount + need["count"]
-
-                if amount != current:
-                    data.add_field(name=item_name, value=str(current)+"/"+str(amount), inline=True)
-                    counter += 1
-                amount = 0
-                item_counter += 1
 
         try:
             await self.bot.say(embed=data)
