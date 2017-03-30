79a66f9f4a1137bad82ec2d23c6a0ea1e2d1a1d2
fabian.ihle <fabian.ihle@hfwu.de>
2017-03-30 14:13:49 +0200

    Check if treasury is empty

--- guildwars2/guildwars2.py
+++ guildwars2/guildwars2.py
@@ -828,20 +828,23 @@ class GuildWars2:
             return
 
         # Collect amounts
-        for item in treasury:
-            if counter < 20:
-                current = item["count"]
-                item_name = itemlist[item_counter]["name"]
-                needed = item["needed_by"]
+        if treasury:
+            for item in treasury:
+                if counter < 20:
+                    current = item["count"]
+                    item_name = itemlist[item_counter]["name"]
+                    needed = item["needed_by"]
 
-                for need in needed:
-                    amount = amount + need["count"]
+                    for need in needed:
+                        amount = amount + need["count"]
 
-                if amount != current:
-                    data.add_field(name=item_name, value=str(current)+"/"+str(amount), inline=True)
-                    counter += 1
-                amount = 0
-                item_counter += 1
+                    if amount != current:
+                        data.add_field(name=item_name, value=str(current)+"/"+str(amount), inline=True)
+                        counter += 1
+                    amount = 0
+                    item_counter += 1
+        else:
+            await self.bot.say("Treasury is empty!")
 
         try:
             await self.bot.say(embed=data)
